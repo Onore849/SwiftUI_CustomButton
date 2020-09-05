bd89ec21b3db654ef1c74b06e6f7bfbf26b584c0
Onore849 <rion3376@gmail.com>
2020-09-05 16:13:26 +0900

    first commit

--- CustomButton/ContentView.swift
+++ CustomButton/ContentView.swift
@@ -10,7 +10,9 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        
+        customButton()
+        
     }
 }
 
@@ -18,4 +20,40 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct customButton : View {
+    
+    @State var count = 0
+    
+    var body: some View {
+        
+        ZStack {
+            
+            Button(action: {
+                
+                print("Clicked")
+                self.count += 1
+                
+            }) {
+                
+                Image(systemName: "bell.fill").resizable().frame(width: 30, height: 30)
+            }.padding()
+                .foregroundColor(Color.white)
+                .background(Color.green)
+                .clipShape(Circle())
+            
+            if count != 0 {
+                
+                Text("\(count)").padding(6).background(Color.red).clipShape(Circle()).foregroundColor(.white).offset(x: 17, y: -25)
+                
+            }
+            
+            
+            
+        }.animation(.spring())
+        
+        
+    }
+    
 }
